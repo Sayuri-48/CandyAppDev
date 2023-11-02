a2c7c7dbac964d4f1d1be199208cdedf97ca7d32
Sayuri-48 <sayuri48@hawaii.edu>
2023-11-02 13:51:05 -1000

    Completed Part 2, Part 10

--- CandyAppDev/secondViewController.swift
+++ CandyAppDev/secondViewController.swift
@@ -12,6 +12,12 @@ class secondViewController: UIViewController, UITableViewDataSource, UITableView
     var cat1List = ["Oreo", "Mother's Cookie", "Kellogg's Scooby Grahams", "Favorite Day"]
     var reviewStars = ["★★★✩✩", "★★★★★", "★★★★✩", "★★★✩✩"]
     
+    var categoryOneImagesData = [String]()
+    
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return cat1List.count
     }
@@ -23,16 +29,35 @@ class secondViewController: UIViewController, UITableViewDataSource, UITableView
                cell.textLabel?.text = text
                return cell
     }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath)
+    {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
 
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         navigationItem.title = "Sayuri's Candy Reviews"
         
-        
+        //Part 8
+        let path = Bundle.main.path(forResource: "Property List", ofType: "plist")
+        let dict = NSDictionary(contentsOfFile: path!)
+        categoryOneImagesData = dict!.object(forKey:"CategoryOneImages") as! [String]
 
         // Do any additional setup after loading the view.
     }
     
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+                if segue.identifier == "mySegue"
+                {
+                    let s1 = segue.destination as! secondDetailViewController
+                    let imageIndex = tableView.indexPathForSelectedRow?.row
+                    s1.imagePass = categoryOneImagesData[imageIndex!]
+                }
+            }
 
 }
